54224457223821c663dd7541a9e589a0ddc2a7b3
Maxim <catlense@voronin.xyz>
2023-08-03 15:46:11 +0700

    Like Button in comment page

--- src/@pages/detailComment.tsx
+++ src/@pages/detailComment.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react";
-import { PageProps, Comment, Response } from "../@types";
+import { PageProps, Comment, Response, User } from "../@types";
 import { MainLayout } from "../@layouts";
 import { FC, useEffect, useState } from "react";
 import { Button, Container, ContainerText, ContainerTitle, Input, Link } from "../@shared";
@@ -8,6 +8,9 @@ import { useParams } from "react-router-dom";
 import { DecisionBlock } from "../@components";
 import { useAuthStoreContext, usePopupStoreContext } from "../@store";
 import { alert } from "../@services/alerting.service";
+import { mdiHeart, mdiHeartOutline } from "@mdi/js";
+import Icon from "@mdi/react";
+import { styled } from "styled-components";
 
 const EditComment: FC<{comment: Comment, setComment: any}> = observer(({comment, setComment}) => {
   const [text, setText] = useState<string>(comment.content);
@@ -27,19 +30,66 @@ const EditComment: FC<{comment: Comment, setComment: any}> = observer(({comment,
   </>;
 });
 
+const ControlButton = styled.button`
+  height: 40px;
+  border: none;
+  background: transparent;
+  width: 100%;
+  border-radius: 8px;
+  font-size: 14px;
+  transition: background 0.2s, transform 0.2s;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  margin-top: 10px;
+  &:hover {
+    background-color: rgba(100, 100, 100, 0.1);
+  }
+  &:active {
+    transform: scale(0.98);
+  }
+`;
+
 const DetailComment: FC<PageProps> = observer(({ title }) => {
   const { id } = useParams();
   const [comment, setComment] = useState<Comment>();
-  const { user } = useAuthStoreContext();
+  const { user, setUser } = useAuthStoreContext();
   const { setVisible, setTitle, setContent} = usePopupStoreContext();
+
+  const [commentLiked, setCommentLiked] = useState<boolean>(false);
 
   useEffect(() => {
     $api.get<Response<Comment>>(`/comment/get?id=${id}`).then(({ data }) => {
       setComment(data.data!);
     });
+    user._id && user.subscribedComments.find((e) => e._id === id) ? setCommentLiked(true) : setCommentLiked(false);
   }, [id]);
 
   if (!comment) return <></>;
+  const newUser = user;
+
+  const subComment = () => {
+    $api.post<Response<User>>("/comment/subscribe", { id }).then(({ data }) => {
+      if (!data.data) return alert("error", "Ошибка", data.message, 15);
+      setCommentLiked(true);
+      newUser.subscribedComments.push(comment);
+      setUser(newUser);
+    });
+  };
+
+  const unSubComment = () => {
+    $api.post<Response<User>>("/comment/unsubscribe", { id }).then(({ data }) => {
+      if (!data.data) return alert("error", "Ошибка", data.message, 15);
+      setCommentLiked(false);
+      newUser.subscribedComments.splice(
+        newUser.subscribedComments.findIndex((e) => e === comment),
+        1,
+      );
+      setUser(newUser);
+    });
+  };
 
   return (
     <MainLayout title={title}>
@@ -60,6 +110,19 @@ const DetailComment: FC<PageProps> = observer(({ title }) => {
           setTitle("Изменение замечания");
           setContent(<EditComment comment={comment} setComment={setComment} />)
         }}>Изменить замечание</Button>}
+        {
+          commentLiked ? (
+            <ControlButton onClick={unSubComment}>
+              <Icon path={mdiHeart} size={"18px"} />
+              Не отслеживать замечание
+            </ControlButton>
+          ) : (
+            <ControlButton onClick={subComment}>
+              <Icon path={mdiHeartOutline} size={"18px"} />
+              Отслеживать замечание
+            </ControlButton>
+          )
+        }
       </Container>
       {comment.decisions && comment.decisions.map((e) => <DecisionBlock decision={e} key={e._id} />)}
     </MainLayout>
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const dev_mode = true;
 
 const config = {
-  dev_title: "Journals v2.1.3 (Link on favorite comments)",
+  dev_title: "Journals v2.1.4 (Like Button in comment page)",
   API: dev_mode ? `http://192.168.0.110:1000/api` : `https://helper.voronin.xyz/api`,
   fileHost: dev_mode ? `http://192.168.0.110:1000/api/public` : `https://helper.voronin.xyz/api/public`,
   fileUpload: dev_mode ? `http://192.168.0.110:1000/api/file/upload` : `https://helper.voronin.xyz/api/file/upload`,
