7fe04b951c01f5803988f751f944ab997ba2c4ac
Maxim <catlense@voronin.xyz>
2023-08-23 20:20:38 +0700

    Layout main developers page

--- src/@pages/developers/index.tsx
+++ src/@pages/developers/index.tsx
@@ -2,11 +2,20 @@ import { observer } from "mobx-react";
 import { PageProps } from "../../@types";
 import { FC } from "react";
 import { DevelopersLayout } from "../../@layouts";
+import { DevTitle, FormText, Link } from "../../@shared";
+import { baseURIs } from "../../config";
 
-const Developers: FC<PageProps> = observer(({title}) => {
-  return <DevelopersLayout title={title}>
-    Welcome
-  </DevelopersLayout>
+const Developers: FC<PageProps> = observer(({ title }) => {
+  return (
+    <DevelopersLayout title={title}>
+      <DevTitle>Добро пожаловать на платформу для разработчиков Helper</DevTitle>
+      <FormText>
+        Здесь Вы можете изучить <Link to={`${baseURIs.developers}/docs/start`}>как начать пользоваться</Link> нашим API в своих проектах, методы и{" "}
+        <Link to={`${baseURIs.developers}/interfaces`}>интерфейсы возвращаемых данных</Link>
+      </FormText>
+      <FormText>Основная навигация находится в сайдбаре слева</FormText>
+    </DevelopersLayout>
+  );
 });
 
 export default Developers;
